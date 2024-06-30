f89050a456c3b644d4be1b6a0e8def1c536fa945
Peter Stoyanov <peterstoyanov83@gmail.com>
2024-06-30 14:18:10 +0300

    Fixed UI tests committing to Junior-dev Branch

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -2,7 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
-  const heading = await page.$('h3');
-  const text = await heading.textContent();
-  expect(text).toBe('About us');
+
+  const footer = await page.waitForSelector('footer', { state: 'attached' });
+  const text = await footer.textContent();
+  expect(text).toContain('Software Engineering and DevOps Retake Exam');
 });
--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,8 +1,10 @@
 const { test, expect } = require('@playwright/test');
 
+
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-a-book');
-    const form = await page.$('list');
-    expect(form).toBeTruthy();
-  });
-  
+  await page.goto('http://localhost:8080/add-book');
+
+  // Check if the form element exists
+  const form = await page.waitForSelector('form', { state: 'attached' });
+  expect(form).toBeTruthy();
+});
